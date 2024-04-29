53181cc42e99e1b55eb9a3d819ffb925420fbed9
Hyunwoo Moon <113423186+m00nhyunw00@users.noreply.github.com>
2024-04-29 17:45:50 +0900

    [Gold I] Title: 트리의 순회, Time: 2404 ms, Memory: 7884 KB -BaekjoonHub

---
+++ 백준/Gold/2263. 트리의 순회/트리의 순회.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#define MAX 100000
+
+using namespace std;
+
+int n;
+int inorder[MAX];       // 중위 순회
+int postorder[MAX];     // 후위 순회
+
+void getPreorder(int inStart, int inEnd, int postStart, int postEnd);
+int searchRootIndex(int target);
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+
+    for (int i=0; i<n; i++)
+        cin >> inorder[i];
+
+    for (int i=0; i<n; i++)
+        cin >> postorder[i];
+
+    getPreorder(0, n-1, 0, n-1);
+
+    return 0;
+}
+
+void getPreorder(int inStart, int inEnd, int postStart, int postEnd) {
+    if (postStart > postEnd || inStart > inEnd)
+        return;
+
+    int inRoot = searchRootIndex(postorder[postEnd]);
+
+    int leftTreeNum = inRoot - inStart;
+    int rightTreeNum = inEnd - inRoot;
+
+    cout << postorder[postEnd] << " ";
+
+    // 왼쪽 서브트리 재귀 호출
+    getPreorder(inStart, inRoot - 1, postStart, postStart + leftTreeNum - 1);
+    // 오른쪽 서브트리 재귀 호출
+    getPreorder(inRoot + 1, inEnd, postStart + leftTreeNum, postEnd - 1);
+}
+
+
+int searchRootIndex(int target){
+    for (int i=0; i<n; i++){
+        if (inorder[i] == target)
+            return i;
+    }
+    return -1;
+}
