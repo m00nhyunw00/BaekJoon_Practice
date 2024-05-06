9ddc33520e6896f2f32a8f50536a26af4cb9cea8
Hyunwoo Moon <113423186+m00nhyunw00@users.noreply.github.com>
2024-05-06 21:55:20 +0900

    [Gold V] Title: 꿀 따기, Time: 8 ms, Memory: 2800 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/21758. 꿀 따기/꿀 따기.cc
+++ 백준/Gold/21758. 꿀 따기/꿀 따기.cc
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <vector>
+#define MAX 100001
 
 using namespace std;
 
 int N;
 int ans = 0;
 int getTotal(int A, int B, int goal);
-vector<int> info;
+int info[MAX];
+int sums[MAX];
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -14,62 +16,32 @@ int main() {
     cout.tie(NULL);
 
     cin >> N;
-    info.resize(N);
 
-    for (int i=0; i<N; i++)
+    for (int i=1; i<=N; i++){
         cin >> info[i];
+        sums[i] = sums[i-1] + info[i];
+    }
 
-    for (int i=0; i<N; i++){
-        int goal = i;
-
-        for (int j=0; j<N; j++){
-            if (j == i)
-                continue;
-            for (int k=0; k<N; k++){
-                if (k == i || k == j)
-                    continue;
-                ans = max(ans, getTotal(j, k, i));
-            }
-        }
+    // 1. [1]꿀통-[i]벌-[N]벌
+    for (int i=2; i<=N-1; i++){
+        int temp = sums[i-1];                      // i번째 벌
+        temp += sums[N-1] - info[i];               // N번째 벌
+        ans = max(ans, temp);
+    }
+    // 2. [1]벌-[i]꿀통-[N]벌
+    for (int i=2; i<=N-1; i++){
+        int temp = sums[i] - sums[1];              // 1번째 벌
+        temp += sums[N-1] - sums[i-1];             // N번째 벌
+        ans = max(ans, temp);
+    }
+    // 1. [1]벌-[i]벌-[N]꿀통
+    for (int i=2; i<=N-1; i++){
+        int temp = sums[N] - sums[1] - info[i];    // 1번째 벌
+        temp += sums[N] - sums[i];                 // i번째 벌
+        ans = max(ans, temp);
     }
 
     cout << ans;
 
     return 0;
-}
-
-int getTotal(int A, int B, int goal){
-    int result = 0;
-
-    if (A < goal){
-        for (int i=A+1; i<=goal; i++){
-            if (i == B)
-                continue;
-            result += info[i];
-        }
-    }
-    else{
-        for (int i=A-1; i>=goal; i--){
-            if (i == B)
-                continue;
-            result += info[i];
-        }
-    }
-
-    if (B < goal){
-        for (int i=B+1; i<=goal; i++){
-            if (i == A)
-                continue;
-            result += info[i];
-        }
-    }
-    else{
-        for (int i=B-1; i>=goal; i--){
-            if (i == A)
-                continue;
-            result += info[i];
-        }
-    }
-
-    return result;
 }
